3309bd134601d2d049b7e2c7608d071bf43bef1c
Rathour1 <43591386+Rathour1@users.noreply.github.com>
2018-10-25 17:14:57 -0400

    Add files via upload

---
+++ RPSGame.py
@@ -0,0 +1,11 @@
+from random import randint
+
+# available weapons => store this in an array
+choices = [" Rock", "Paper", "Scissors"]       
+player = False 
+
+# make the computer pick one item at random
+computer_choice = choices[randint(0, 2)]
+
+# show the computer's choices in the terminal window
+print("Computer chooses: ", computer_choices)
---
+++ conditions.py
@@ -0,0 +1,7 @@
+print("this is the conditions file")
+
+current_temp = False
+
+while current_temp is False:
+	 current_temp = input("Enter a temprature: \n")
+	 print(current_temp)
