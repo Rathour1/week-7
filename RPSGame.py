daf96d502ec6d781d7a417be5fff4284d5031310
Singh <k_singh74131@tss-hr420-dm03.fc.ca>
2018-11-01 16:10:01 -0400

    dev.updatereadme

--- RPSGame.py
+++ RPSGame.py
@@ -1,11 +1,51 @@
 from random import randint
 
-# available weapons => store this in an array
-choices = [" Rock", "Paper", "Scissors"]       
-player = False 
+#create a List of play options
+choices = ["Rock", "Paper", "Scissors"]
+player = False
 
-# make the computer pick one item at random
-computer_choice = choices[randint(0, 2)]
+#assign a random play to the computer
+computer = choices[randint(0,2)]
 
-# show the computer's choices in the terminal window
-print("Computer chooses: ", computer_choices)
+#show the computer's choice in the terminal window
+print("Computer chooses:" , computer)
+
+player = 5
+computer = 5
+
+while player == False:
+#set player to True
+    print("Choose your weapon!")
+    player = input("Rock, Paper, Scissors?")
+    
+    print(player, "\n")
+
+    if player == computer:
+    	print("Tie!")
+    
+    elif player == "Rock":
+    	if computer == "Paper":
+    		print("you Lose !", computer, "covers", player)
+    	else:
+    		print("You Win!", player, "smashes", computer)
+   
+    elif player == "Paper":
+    	if computer == "Scissors":
+    		print("You Lose!", computer, "cut", player)
+    	else:
+    		print("You win!", player, "covers", computer)
+   
+    elif player == "Scissors":
+    	if computer == "Rock":
+    		print("You lose...", computer, "smashes", player)
+    	else:
+    		print("You win!", player, "cut", computer)
+    elif player == "quit":
+        exit()
+    else:
+    	print("That's not a valid play. Check your spelling!\n")
+
+    #reset the game loop and start again
+    #player was set to True, but we want it to be false so the loop continues
+    player = False
+    computer = choices[randint(0,2)]
